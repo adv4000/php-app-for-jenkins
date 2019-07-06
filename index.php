d7edaaee7f01c686f60c36c5319e8d743dc529af
Denis Astahov <adv4000@gmail.com>
2019-07-06 12:24:37 -0700

    Version 2.1

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1><font color=red>Congratulations <font color=gold>DevOps Engineer!!!</font></h1>
+        <h1><font color=red>Congratulations <br><font color=gold>DevOps Engineer!!!</font></h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
